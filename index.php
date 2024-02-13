62a33a601943482310205e9d8e3a1409c61b05c2
NASIM-PP <nasim@veuz.in>
2024-02-13 10:48:39 +0530

    first push

---
+++ index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Veuz Marketing</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+  <section class="home-banner">
+
+    <div class="main-banner">
+      <div class="overlay"></div>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light main-nav">
+          <a class="navbar-brand" href="#">
+            <img src="assets/img/logo-white.svg" alt="">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse w-100 d-flex justify-content-between navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mr-auto home-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#">Why Choose Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#">Contact Us</a>
+              </li>
+            </ul>
+
+            <button class="btn btn-outline-success nav-btn my-2 my-sm-0" type="submit">Get started now</button>
+
+          </div>
+        </nav>
+
+        <div class="banner-cntnt">
+          <div class="container">
+            <h2>Get help from the expert <br>
+              consultants.</h2>
+            <p>With lots of unique blocks, you can easily build a page without coding. Build your next <br> consultancy
+              website within few minutes.</p>
+            <button class="main-btn" type="button">About us <span><i
+                  class="fa-solid fa-arrow-right-long"></i></span></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3">
+          <div class="counter-box">
+            <h2><span id="Partners">1000</span>+</h2>
+            <h3>HAPPY CUSTOMERS</h3>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="counter-box">
+            <h2><span id="Projects">0</span>+</h2>
+            <h3>YEARS EXPERIENCE</h3>
+          </div>
+
+        </div>
+        <div class="col-lg-3">
+          <div class="counter-box">
+            <h2><span id="Clients">0</span>+</h2>
+            <h3>EMPLOYEES</h3>
+          </div>
+
+        </div>
+        <div class="col-lg-3">
+          <div class="counter-box">
+            <h2><span id="branches">0</span></h2>
+            <h3>BRANCHES</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="section-padding">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+        <div class="col-lg-6">
+          <h5 class="secdry-sub-head">
+            Core Features
+          </h5>
+
+          <h2 class="main-sub-head">
+            Write ✍️ Better Content Faster,
+            Make you Life Easier.
+          </h2>
+          <p class="main-para">Designed to streamline the entire event management process, saving
+            you valuable time and effort. With our software, you can focus on what truly
+            matters: creating memorable experiences for your attendees.
+          </p>
+
+          <p class="main-para point-para">Designed to streamline the entire event management process, saving
+            you valuable time and effort.
+          </p>
+
+          <button class="main-btn" type="button">Download Brochure <span><i
+                class="fa-solid fa-download"></i></span></button>
+        </div>
+        <div class="col-lg-6">
+          <img src="assets/img/abt-img.png" alt="" class="abt-img">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-padding">
+    <div class="container">
+      <div class="text-center-div">
+        <h2 class="main-sub-head">
+          Services we <span>offer for you</span>
+        </h2>
+        <p class="main-para">With lots of unique blocks, you can easily build a page without coding. Build your next
+          landing page.
+        </p>
+      </div>
+      <div class="row mt-50">
+        <div class="col-lg-4">
+          <div class="servce-div">
+            <img src="assets/img/service/srvce-1.png" alt="" class="img-fluid">
+            <h4>Website Design</h4>
+            <a href="">Get a free quote <span><i class="fa-solid fa-arrow-right"></i></span></a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="servce-div">
+            <img src="assets/img/service/srvce-2.png" alt="" class="img-fluid">
+            <h4>Website Design</h4>
+            <a href="">Get a free quote <span><i class="fa-solid fa-arrow-right"></i></span></a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="servce-div">
+            <img src="assets/img/service/srvce-3.png" alt="" class="img-fluid">
+            <h4>Website Design</h4>
+            <a href="">Get a free quote <span><i class="fa-solid fa-arrow-right"></i></span></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mt-50">
+        <div class="col-lg-4">
+          <div class="servce-div">
+            <img src="assets/img/service/srvce-1.png" alt="" class="img-fluid">
+            <h4>Website Design</h4>
+            <a href="">Get a free quote <span><i class="fa-solid fa-arrow-right"></i></span></a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="servce-div">
+            <img src="assets/img/service/srvce-2.png" alt="" class="img-fluid">
+            <h4>Website Design</h4>
+            <a href="">Get a free quote <span><i class="fa-solid fa-arrow-right"></i></span></a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="servce-div">
+            <img src="assets/img/service/srvce-3.png" alt="" class="img-fluid">
+            <h4>Website Design</h4>
+            <a href="">Get a free quote <span><i class="fa-solid fa-arrow-right"></i></span></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="text-center-div mt-40">
+        <button class="main-btn" type="button">View More<span><i
+              class="fa-solid fa-arrow-right-long"></i></span></button>
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <section class="section-padding">
+    <div class="container">
+      <div class="text-center-div">
+        <h5 class="secdry-sub-head">
+          Offer highlights
+        </h5>
+        <h2 class="main-sub-head">
+          Services we <span>offer for you</span>
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="offer-box">
+            <h5 class="offer-plan">Basic Plan</h5>
+            <h3 class="offer-head">Microsoft 365 Plan</h3>
+            <h6 class="offer-per-month">Per Month</h6>
+            <hr>
+            <ul class="offer-list offer-cntnt-dark">
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+            </ul>
+            <div class="text-center-div">
+              <button class="enquiry-btn" type="button">Enquiry</button>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="offer-box-dark">
+            <div class="best-plan">
+              Best plan
+            </div>
+            <h5 class="offer-plan offer-white">Basic Plan</h5>
+            <h3 class="offer-head offer-white">Microsoft 365 Plan</h3>
+            <h6 class="offer-per-dark">Per Month</h6>
+            <hr>
+            <ul class="offer-list offer-cntnt-white">
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+            </ul>
+            <div class="text-center-div">
+              <button class="enquiry-btn" type="button">Enquiry</button>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="offer-box">
+            <h5 class="offer-plan">Basic Plan</h5>
+            <h3 class="offer-head">Microsoft 365 Plan</h3>
+            <h6 class="offer-per-month">Per Month</h6>
+            <hr>
+            <ul class="offer-list offer-cntnt-dark">
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+              <li>Lorem Ipsum is simply dummy text of
+                the printing</li>
+            </ul>
+            <div class="text-center-div">
+              <button class="enquiry-btn" type="button">Enquiry</button>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+  </script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="https://kit.fontawesome.com/16b0815225.js" crossorigin="anonymous"></script>
+  <script src="assets/js/script.js"></script>
+
+
+  <script>
+    // this function used to creat th countUp  animation for business websites
+    // use the three parameters to customize it
+    // call it for every element
+    // note that may be small diffrance in the count 100 be 99 so chack it
+    function numCounter(tagId, maxCount, speed) {
+      var initialNumber = 0;
+
+      function counter() {
+        document.getElementById(tagId).innerHTML = initialNumber;
+        ++initialNumber;
+      }
+      var counterDelay = setInterval(counter, speed);
+
+      function totalTime() {
+        clearInterval(counterDelay);
+      }
+      var totalPeriod = speed * maxCount;
+      setTimeout(totalTime, totalPeriod);
+    }
+
+    numCounter("Projects", 15, 100);
+    numCounter("Clients", 301, 15);
+    numCounter("Partners", 1001, 10);
+    numCounter("branches", 1001, 10);
+  </script>
+</body>
+
+</html>
